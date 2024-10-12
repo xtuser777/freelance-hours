80a049129cce1769f8e11d517f705e5bd08f08c1
Lucas <lucaoxt@gmail.com>
2024-10-11 23:06:59 -0300

    aula 03

--- resources/views/components/projects/card.blade.php
+++ resources/views/components/projects/card.blade.php
@@ -15,20 +15,8 @@
             </div>
         </div>
         <div>
-            <button class="bg-[#5354FD] text-white font-bold tracking-wide uppercase px-8 py-3 rounded-[4px]
-                    hover:bg-[#1f20a6] transition duration-300 ease-in-out"
-                @click="$refs.modal.showModal(); showModal= true">
-                Enviar uma proposta
-            </button>
-            <div class="flex items-center justify-between mt-4 text-[14px]">
-                <div class="text-[#8C8C9A]  leading-6">Encerra em:</div>
-                <div class="font-bold flex items-center space-x-1">
-                    <span class="text-white ">01</span><span>:</span>
-                    <span class="text-white ">12</span><span>:</span>
-                    <span class="text-white ">26</span><span>:</span>
-                    <span class="text-white ">64</span>
-                </div>
-            </div>
+            <livewire:proposals.create :$project />
+            <livewire:projects.timer :$project />
         </div>
     </div>
 
